2ba4b8f84ddc4305aa8695381956519cc00e789f
Gérard Lichtert <gerard.lichtert@hotmail.com>
2023-11-09 14:33:11 +0100

    Implemented play/0 and added exports

--- game_logic.pl
+++ game_logic.pl
@@ -1,2 +1,15 @@
-:- use_module([library(lists), io, fill, board]).
+:- use_module([library(lists), io, fill, board, winner]).
+:- module(game_logic, []).
+
+
+play :- 
+    welcome, 
+    initial_board(Board),
+    display_board(Board),
+    is_black(Black).
+    % TODO implement the predicate below:
+    % play(Black, Board).
+
+
+
 
