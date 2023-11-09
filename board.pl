01243b3bfc0dfd2f68c0919ea5a2c890fe6581d8
Gérard Lichtert <gerard.lichtert@hotmail.com>
2023-11-09 14:07:17 +0100

    implemented count_pieces predicate

--- board.pl
+++ board.pl
@@ -89,3 +89,14 @@ empty_board([
     [HA,HB,HC,HD,HE,HF,HG,HH]
     ]).
 
+count_pieces_row([], 0, 0).
+count_pieces_row([Head | Tail], Black, White) :-
+    is_black(Head), count_pieces_row(Tail, PrevBlack, White), Black is PrevBlack + 1;
+    is_white(Head), count_pieces_row(Tail, Black, PrevWhite), White is PrevWhite + 1;
+    count_pieces_row(Tail, Black, White).
+count_pieces([], 0, 0).
+count_pieces([Head | Tail], Black, White) :- 
+    count_pieces_row(Head, RowBlack, RowWhite),
+    count_pieces(Tail, RestBlack, RestWhite),
+    Black is RowBlack + RestBlack,
+    White is RowWhite + RestWhite.
