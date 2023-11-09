24457c35dfa0b748423a12cd37c9e96a55a53391
Gérard Lichtert <gerard.lichtert@hotmail.com>
2023-11-09 16:19:23 +0100

    Progress on enclosing_piece

--- game_logic.pl
+++ game_logic.pl
@@ -10,7 +10,21 @@ play :-
     % TODO implement the predicate below:
     % play(Black, Board).
 
-% enclosing_piece(X, Y, Player, Board, U, V, N) :-
+
+/* match_list(First, Last, N, Piece, [Head | Tail]) :- 
+    First = 1, match_list(1, Last, N, Piece, [Head | Tail]);
+    FirstSucc is First - 1, 
+    match_list(FirstSucc, Last, N, Piece, Tail).
+
+
+enclosing_piece(X, Y, Player, Board, U, Y, N) :-
+    row(Y, Board, row(Y, A, B, C, D, E, F, G, H)),
+    U < X, match_list(U, X, Player, N, [A, B, C, D, E, F, G, H]);
+    X < U, match_list(X, U, Player, N, [A, B, C, D, E, F, G, H]).
+
+
+
+ */
 
 
 
